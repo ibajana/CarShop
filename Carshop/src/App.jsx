884509424ace6329514c34362ab01e54a00af6bc
ibajana <isaiasbajana@gmail.com>
2024-04-25 16:55:09 -0500

    focus on client component

--- Carshop/src/App.jsx
+++ Carshop/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
 
   return (
@@ -10,14 +11,11 @@ function App() {
       <DarkButton></DarkButton>
       <h1>RECEPCION DE AUTOS</h1>
       <hr />
-      <h2>Datos del cliente</h2>
-    <Form>
-      <Client></Client>
-    </Form>
       
-      <Vehicle />
-      <Services />
-
+      <Form>
+        <Client>
+        </Client>
+      </Form>
     </>
   )
 }
@@ -28,102 +26,104 @@ function DarkButton() {
   )
 }
 
+
+
 function Form({ children }) {
+  const [complete,setcomplete]=useState(false)
+  const handleComplete = ()=>{
+    setcomplete(true)
+  }
   return (
     <>
-      <form action="" id='formulario'>
+      <form id='formulario' handleComplete={handleComplete}>
         {children}
       </form>
-
     </>
   )
 }
 
 function Client() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("evitando el submit")
+  }
   const [name, Setname] = useState("")
   const [email, Setemail] = useState("")
   const [phone, Setphone] = useState("")
-  const handleSubmmit=(
-  (e)=>{}
-  )
 
   return (
     <div className='container'>
+      <h2>Datos del cliente</h2>
       <label>Nombre del cliente:</label>
       <input type="text" value={name} onChange={(e) => { Setname(e.target.value) }} />
       <label>Email:</label>
       <input type="email" value={email} onChange={(e) => { Setemail(e.target.value) }} />
-        <label>Numero del cliente:
-          <input type='text' value={phone} onChange={(e) => { Setphone(e.target.value) }} />
-        </label>
-        <button type="submit" onSubmit={handleSubmmit}>Siguiente</button>
+      <label>Numero del cliente:</label>
+      <input type='text' value={phone} onChange={(e) => { Setphone(e.target.value) }} />
+      <button type="submit" onClick={handleSubmit}>Siguiente</button>
     </div>
-        
-    
   )
 }
 
 function Vehicle() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("evitando el submit")
+  }
   return (
-    <>
+    <div className='container' >
       <h2>Datos del Vehículo</h2>
-      <form action="">
-        <label htmlFor="">Marca:
-          <input type="text" name="" id="" />
-        </label>
-        <label htmlFor="">Modelo:
-          <input type="text" name="" id="" />
-        </label>
-        <label htmlFor="">Placa:
-          <input type="text" name="" id="" />
-        </label>
-        <label htmlFor="">Nivel de Gasolina:
-          <input type="text" name="" id="" />
-        </label>
-        <label htmlFor=""></label>
-        <label htmlFor=""> Observaciones
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </label>
-      </form>
-    </>
+      <label >Marca:</label>
+      <input type="text" />
+      <label>Modelo:</label>
+      <input type="text" />
+      <label >Placa:</label>
+      <input type="text" />
+      <label >Nivel de Gasolina:</label>
+      <input type="text" />
+      <label > Observaciones</label>
+      <textarea cols="30" rows="10"></textarea>
+      <button type="submit" onClick={handleSubmit}>Siguiente</button>
+    </div>
   )
 }
 
 function Services() {
   return (
-    <>
+    <div>
       <h2>Servicios Disponibles</h2>
-      <form action="">
         <div>
-          <input class="form-check-input" type="checkbox" id="opcion1" value="Aceite" />
-          <label for="opcion1" class="form-check-label">Cambio de Aceite</label>
+          <input type="checkbox" value="Aceite" />
+          <label >Cambio de Aceite</label>
+          
         </div>
         <div>
-          <input class="form-check-input" type="checkbox" id="opcion1" value="Freno" />
-          <label for="opcion1" class="form-check-label">Cambio de frenos</label>
+          <input type="checkbox" value="Freno" />
+          <label >Cambio de frenos</label>
         </div>
         <div>
-          <input class="form-check-input" type="checkbox" id="opcion1" value="Alineacion" />
-          <label for="opcion1" class="form-check-label">Alineacion</label>
+          <input type="checkbox" value="Alineacion" />
+          <label >Alineacion</label>
         </div>
         <div>
-          <input class="form-check-input" type="checkbox" id="opcion1" value="Balanceo" />
-          <label for="opcion1" class="form-check-label">Balanceo</label>
+          <input type="checkbox" value="Balanceo" />
+          <label >Balanceo</label>
         </div>
         <div>
-          <input class="form-check-input" type="checkbox" id="opcion1" value="Diagnostico" />
-          <label for="opcion1" class="form-check-label">Diagnostico general</label>
+          <input type="checkbox" value="Diagnostico" />
+          <label >Diagnostico general</label>
         </div>
         <div>
-          <input class="form-check-input" type="checkbox" id="opcion1" value="Electrico" />
-          <label for="opcion1" class="form-check-label">Revision sistema electrico</label>
+          <input type="checkbox" value="Electrico" />
+          <label >Revision sistema electrico</label>
         </div>
         <div>
-          <input class="form-check-input" type="checkbox" id="opcion1" value="Suspension" />
-          <label for="opcion1" class="form-check-label">Revisión de la suspensión</label>
+          <input type="checkbox" value="Suspension" />
+          <label >Revisión de la suspensión</label>
         </div>
-      </form>
-    </>
+    </div>
   )
 }
 
