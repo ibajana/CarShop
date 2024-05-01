200dbf46b4e9ccd8133720fdb397038c2d9e1bab
ibajana <isaiasbajana@gmail.com>
2024-04-30 23:54:16 -0500

    first implemented use context

--- Carshop/src/App.jsx
+++ Carshop/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import React from 'react'
+import { useState, useContext } from 'react'
 import { useForm } from "react-hook-form"
 import './App.css'
+
 
 
 function App() {
 
   return (
     <>
-      <Button text='Modo Oscuro' handle={handleConsole}></Button>
+      <button >Modo Oscuro</button>
       <h1>RECEPCION DE AUTOS</h1>
       <hr />
       <Form>
@@ -18,16 +20,9 @@ function App() {
   )
 }
 
-const handleConsole = () => { console.log("hola") }
-
-function Button({ text = "I'm a button change my name with my prop (text)", handle }) {
-  return (
-    <button onClick={handle}>{text}</button>
-  )
-}
 
 
-
+// eslint-disable-next-line react/prop-types
 function Form({ children }) {
 
   return (
@@ -40,7 +35,11 @@ function Form({ children }) {
   )
 }
 
+
+const clientContext = React.createContext();
+
 function Client() {
+
   const [complete, setComplete] = useState(true)
   const {
     register,
@@ -53,7 +52,7 @@ function Client() {
   const phone=getValues("phone")
   const email=getValues("email")
   const id=getValues("id")*/
-  const datosClient= getValues()
+  const datosClient = getValues()
 
   const submitfunc = handleSubmit(
     (data) => {
@@ -72,9 +71,10 @@ function Client() {
       {errors.name?.type === "minLength" && (<span>El nombre debe tener al menos dos caracteres</span>)}
 
       <label>*Email:</label>
-      <input type="email" autoComplete='off' 
-      {...register("email",{required:true,
-        pattern:/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+      <input type="email" autoComplete='off'
+        {...register("email", {
+          required: true,
+          pattern: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
         })} />
       {errors.email?.type === "required" && (<span>El email es obligatorio</span>)}
       {errors.email?.type === "pattern" && (<span>El email introducido no es válido</span>)}
@@ -86,17 +86,24 @@ function Client() {
 
 
       <label>*Identificación:</label>
-      <input type='text' {...register("id",{required:true})} />
+      <input type='text' {...register("id", { required: true })} />
       {errors.id?.type === "required" && (<span>El Id del cliente es obligatorio</span>)}
 
 
-      <Button text='Siguiente' handle={submitfunc}></Button>
+      <button onClick={submitfunc}>Siguiente</button>
     </div> :
-    <Vehicle datosClient={datosClient} ></Vehicle>
+    <>
+      <clientContext.Provider value={datosClient}>
+        <Vehicle></Vehicle>
+      </clientContext.Provider>
+    </>
+
   )
 }
 
-function Vehicle({datosClient}) {
+const vehicleContext = React.createContext();
+
+function Vehicle() {
   const [completeVehicle, setCompleteVehicle] = useState(true)
   const {
     register,
@@ -105,7 +112,7 @@ function Vehicle({datosClient}) {
     formState: { errors }
   } = useForm()
 
-  const datosVehicle= getValues()
+  const datosVehicles = getValues()
 
   const submitfunc = handleSubmit(
     (data) => {
@@ -113,55 +120,61 @@ function Vehicle({datosClient}) {
       setCompleteVehicle(false)
     }
   )
-  
+
   return (
     <div className='container' >
       {completeVehicle ?
         <>
           <h2>Datos del Vehículo</h2>
           <label >*Marca:</label>
-          <input type="text" {...register("brand",{required:true})}/>
-          {errors.brand?.type==="required"&&(<span>La marca del vehiculo es obligatoria</span>)}
+          <input type="text" {...register("brand", { required: true })} />
+          {errors.brand?.type === "required" && (<span>La marca del vehiculo es obligatoria</span>)}
 
 
           <label>*Modelo:</label>
-          <input type="text" {...register("model",{required:true})} />
-          {errors.model?.type==="required"&&(<span>El modelo del vehiculo es obligatorio</span>)}
+          <input type="text" {...register("model", { required: true })} />
+          {errors.model?.type === "required" && (<span>El modelo del vehiculo es obligatorio</span>)}
 
 
           <label>*Placa:</label>
-          <input type="text" {...register("placa",{required:true})} />
-          {errors.placa?.type==="required"&&(<span>La placa del vehiculo es obligatoria</span>)}
+          <input type="text" {...register("placa", { required: true })} />
+          {errors.placa?.type === "required" && (<span>La placa del vehiculo es obligatoria</span>)}
 
 
           <label>*Nivel de Gasolina:</label>
-          <input type="text" {...register("gas",{required:true})} />
-          {errors.gas?.type==="required"&&(<span>El nivel de gasolina del vehiculo es obligatorio</span>)}
+          <input type="text" {...register("gas", { required: true })} />
+          {errors.gas?.type === "required" && (<span>El nivel de gasolina del vehiculo es obligatorio</span>)}
 
 
           <label>Observaciones</label>
-          <textarea cols="30" rows="10" {...register("observation",{maxLength:150})}></textarea>
-          <Button text='Siguiente' handle={submitfunc} />
-        </> :
-        <Services datosClient={datosClient} datosVehicle={datosVehicle} />
+          <textarea cols="30" rows="10" {...register("observation", { maxLength: 150 })}></textarea>
+          <button onClick={submitfunc}>Siguiente</button>
+        </> 
+        :
+        <>
+          <vehicleContext.Provider value={datosVehicles}>
+            <Services></Services>
+          </vehicleContext.Provider>
+        </>
       }
     </div>
   )
 }
 
-function Services({datosClient,datosVehicle}) {
+const serviceContext = React.createContext()
 
-  const[completeServices,setCompleteServices]=useState(true)
+function Services() {
+
+  const [completeServices, setCompleteServices] = useState(true)
 
   const {
     register,
     getValues,
     handleSubmit,
-    formState: { errors }
   } = useForm()
 
   const datoServices = getValues()
-  
+
   const handlePrint = handleSubmit(
     (data) => {
       console.log(data)
@@ -171,41 +184,47 @@ function Services({datosClient,datosVehicle}) {
 
   return (completeServices?
     <div>
-      <p>la marca es:{datosVehicle.brand}</p>
       <h2>Servicios Disponibles</h2>
       <input type="checkbox" {...register("oil")} />
       <label >Cambio de Aceite</label>
 
-      <input type="checkbox" {...register("frenos")}/>
+      <input type="checkbox" {...register("frenos")} />
       <label >Cambio de frenos</label>
-      
-      <input type="checkbox" {...register("alineacion")}/>
+
+      <input type="checkbox" {...register("alineacion")} />
       <label >Alineacion</label>
 
-      <input type="checkbox" {...register("balanceo")}/>
+      <input type="checkbox" {...register("balanceo")} />
       <label >Balanceo</label>
 
-      <input type="checkbox" {...register("generalD")}/>
+      <input type="checkbox" {...register("generalD")} />
       <label >Diagnostico general</label>
 
-      <input type="checkbox" {...register("electrico")}/>
+      <input type="checkbox" {...register("electrico")} />
       <label >Revision sistema electrico</label>
 
-      <input type="checkbox" {...register("suspension")}/>
+      <input type="checkbox" {...register("suspension")} />
       <label >Revisión de la suspensión</label>
 
       <button onClick={handlePrint}>Imprimir Datos Finales</button>
-    </div>:
-    <FinalPrint datoServices={datoServices} datosClient={datosClient} datosVehicle={datosVehicle}/>
+    </div> :
+    <>
+      <serviceContext.Provider value={datoServices}>
+        <FinalPrint />
+      </serviceContext.Provider>
+    </>
+  
   )
 }
 
-function FinalPrint({datosClient,datosVehicle,datoServices}){
-  
-  return(
+function FinalPrint() {
+  const datosClient = useContext(clientContext)
+  /*const datosVehicle = useContext(vehicleContext)
+  const datosServices = useContext(serviceContext)*/
+  return (
     <>
       <h2>Detalle del Registro</h2>
-      <p>El nombre del cliente es:{datosClient.name}</p>
+      <p>El nombre del cliente es: {datosClient.name}</p>
     </>
   )
 }
