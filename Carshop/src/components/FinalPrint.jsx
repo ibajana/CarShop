3649c0e51bab9dcf3863f5ad44ca64ec121f57b5
ibajana <isaiasbajana@gmail.com>
2024-05-03 09:40:50 -0500

    changes in FinalPrint

--- Carshop/src/components/FinalPrint.jsx
+++ Carshop/src/components/FinalPrint.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { clientContext }from './clientContext'
 import { vehicleContext } from './vehicleContext';
 import { serviceContext } from './serviceContext';
+import { Button } from 'react-bootstrap';
 
 
 
@@ -18,12 +19,18 @@ export function FinalPrint() {
 
   return (
     <>
-      <h2>Detalle del Registro</h2>
-      <p>El nombre del cliente es: {datosClient.name}</p>
+      <h1 className='fw-bold'>DETALLE DEL REGISTRO</h1>
+      <h2>Datos del Cliente</h2>
+      <p>Cliente: {datosClient.name}</p>
+      <p>Celular {datosClient.phone}</p>
+      <p>Correo: {datosClient.email}</p>
+      <p>Identificacion: {datosClient.idType} #{datosClient.id}</p>
+      <h2 className='fw-bold'>Vehiculo: {datosVehicle.name}</h2>
+
       <p>El modelo del vehiculo es: {datosVehicle.brand}</p>
       <p>Servicios a realizar:</p>
       {trueServices.map((service)=><ul><li>{service}</li></ul>)}
-      
+      <Button>Confirmar Registro</Button>
     </>
   );
 }
