3814f2bbcdc885acdc7484d6aaf3c7ee0dc28633
ibajana <isaiasbajana@gmail.com>
2024-05-14 11:38:22 -0500

    Trying to find the error en back ejs

--- Back/index.js
+++ Back/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello World not gay 3001');
+const engine = require('ejs-locals');
+
+
+
+app.engine('ejs', engine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 
-app.listen(3001, () => console.log('Example app listening on port 3000!'))
+app.get('/dist', (req, res) => {
+    res.render('dist/index.ejs');
+});
+
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
